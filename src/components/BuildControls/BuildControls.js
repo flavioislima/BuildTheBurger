7a9bc00915269fb100e579f253a1e5b902d10731
Flavio Lima <flavioislima@gmail.com>
2018-11-04 16:02:50 -0300

    Added Components to add ingredients

---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from './BuildControls.css'
+import Control from './Control/Control'
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Meat', type: 'meat' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+]
+
+const BuildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {
+      controls.map(ctrl => <Control key={ctrl.label} label={ctrl.label} />)
+    }
+  </div>
+)
+
+export default BuildControls
